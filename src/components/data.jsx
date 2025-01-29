9c37ca4db18af925415e9798a4b29c9599a13fc3
Precise Goals <elitesarthakamr@gmail.com>
2025-01-30 00:38:43 +0530

    hackathon showcase

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -6,18 +6,19 @@ export const hackathonData = [
       "Join the Falcons Programming Competition on December 7th, 2024! Challenge yourself in C/C++, Java, and Python, showcase your coding skills",
     img: "https://s3.amazonaws.com/hr-assets/0/1733574900-085c527931-F1.png",
     date: "7th December",
-    color: "https://i.pinimg.com/originals/cf/2c/a4/cf2ca4f35eff08e2d8724e2e4b5cdf42.gif",
+    color:
+      "https://i.pinimg.com/originals/51/4f/3f/514f3fccb71047d780be491c435a79e1.gif",
     url: "https://www.hackerrank.com/falcons-vanheimn",
   },
   {
-    id: "1",
+    id: "2",
     hackTitle: "Falcons MiniHackon",
     hackDesc:
       "Join us for the Falcons Minihack Challenge, a small yet exciting coding contest designed to test your programming knowledge and skills.",
     img: "https://s3.amazonaws.com/hr-assets/0/1733574900-085c527931-F1.png",
-    date: "7th December",
+    date: "14th December",
     color:
       "https://steamuserimages-a.akamaihd.net/ugc/802114790894531555/045F7A07B4272EF961134A63045A60BCB9A4CEC3/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-    url: "https://www.hackerrank.com/falcons-vanheimn",
+    url: "https://www.hackerrank.com/falcons-minihack",
   },
 ];
