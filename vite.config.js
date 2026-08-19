759bfa7bf9b2f705e61657c8283c4a0cb399f159
Precise-Goals <elitesarthakamr@gmail.com>
2026-08-20 03:08:01 +0530

    surprise loaded

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
-    outDir: "dist",
-    optimizeDeps: {
-      include: ["tslib", "react-router-dom", "express/lib/response"],
-    },
-  },
 });
